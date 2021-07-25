76b5e9a0e7f0328209a9362d1b3b6cd435debdd4
Olga Wojtaszko <olga.jackowska@wp.pl>
2021-07-25 17:26:03 +0200

    1. sprawdzenie poprawności wprowadzonej kombinacji 2. znajdowanie kolorów na swoim miejscu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-colors = ["blue", "green", "red", "yellow", "black", "white"]
+colors = ['blue', 'green', 'red', 'yellow', 'black', 'white']
 code = []
 
 lenCode = 0
@@ -8,20 +8,34 @@ while lenCode < 4:
     code.append(random.choice(colors))
     lenCode += 1
 
-#print(code)
-
-print("Odgadnij kod i zostań Mistrzem Intelektu!\n"
-      "Twoim zadaniem jest jak najszybsze odgadnięcie kodu, który jest kombinacją ustawienia w szeregu, 4 spośród 6 kolorów, z możliwymi powtórzeniami.\n"
-      "Wprowadź swoją pierwszą kombinację, a otrzymasz podpowiedzi, które przybliżą Cię do odgadnięcia kodu. Czas Start! ")
+#print('Kod', code)
+print('Odgadnij kod i zostań Mistrzem Intelektu!\n'
+      'Twoim zadaniem jest jak najszybsze odgadnięcie kodu, który jest kombinacją ustawienia w szeregu, 4 spośród 6 kolorów, z możliwymi powtórzeniami.\n'
+      'Wprowadź swoją pierwszą kombinację kolorów: blue, green, red, yellow, black, white, \n'
+      'a otrzymasz podpowiedzi, które przybliżą Cię do odgadnięcia kodu w kolejnych kombinacjach. Powodzenia. Czas Start! ')
 
 userCode = []
 i = 0
 while i < 4:
-    part = input("Podaj kolor\n")
-    userCode.append(part)
-    i += 1
+    part = input('Podaj kolor\n')
 
-#print(userCode)
+    if part != 'blue' and part != 'green' and part != 'red' and part != 'yellow' and part != 'black' and part != 'white':
+        print('Wybrano błędny kolor.')
+    else:
+        userCode.append(part)
+        i += 1
+
+for x in userCode:
+    i = 0
+    redPoint: int = 0
+    while i < 4:
+        if userCode[i] == code[i]:
+            redPoint += 1
+        i += 1
+
+print('Sprawdzana kombinacja', userCode)
+print('Ilość kolorów na swoim miejscu', redPoint)
+
 
 
 
